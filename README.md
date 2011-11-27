cd4cc582f3f6d4795973c3bb66b8b17efca35b0c
Calle Laakkonen <calle@luolamies.org>
2011-11-27 19:15:09 +0200

    Added license and readme.

---
+++ README.md
@@ -0,0 +1,5 @@
+KQueryBrowser
+==============
+
+KQueryBrowser is a database query tool for KDE.
+Currently, SQLite, MySQL and PostgreSQL are supported.
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+//
+// This file is part of KQueryBrowser.
+//
+// KQueryBrowser is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// KQueryBrowser is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with KQueryBrowser.  If not, see <http://www.gnu.org/licenses/>.
+//
 #include <QDebug>
 #include <QUrl>
 
@@ -34,7 +50,7 @@ int main (int argc, char *argv[])
 				ki18n("(c) 2011"),
 				// Optional text shown in the About box.
 				// Can contain any information desired.
-				"",
+				ki18n(""),
 				// The program homepage string.
 				"http://luolamies.org/software/kquerybrowser",
 				// The bug report email address
