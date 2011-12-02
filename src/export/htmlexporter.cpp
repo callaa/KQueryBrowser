eb04ccd33246cc283870c0fa2946a766a3938dab
Calle Laakkonen <calle@luolamies.org>
2011-12-02 20:23:14 +0200

    Use template for HTML export

--- src/export/htmlexporter.cpp
+++ src/export/htmlexporter.cpp
@@ -1,4 +1,7 @@
 #include <QVariant>
+#include <QFile>
+
+#include <KStandardDirs>
 
 #include "exporter.h"
 #include "../meta/column.h"
@@ -6,6 +9,7 @@
 class HtmlExporter : public Exporter {
 public:
 	HtmlExporter();
+	~HtmlExporter() { delete m_tpl; }
 
 	void startFile(QIODevice *file);
 	void beginTable(TableCellIterator *iterator);
@@ -13,6 +17,7 @@ public:
 
 private:
 	QIODevice *m_file;
+	QFile *m_tpl;
 };
 
 class HtmlExporterFactory : public ExporterFactory
@@ -35,7 +40,14 @@ HtmlExporter::HtmlExporter()
 void HtmlExporter::startFile(QIODevice *file)
 {
 	m_file = file;
-	m_file->write("<!DOCTYPE html>\n<html><body>\n");
+	m_tpl = new QFile(KStandardDirs::locate("appdata", "export.html"));
+	m_tpl->open(QIODevice::ReadOnly | QIODevice::Text);
+	while(true) {
+		QByteArray line = m_tpl->readLine();
+		if(line.isEmpty() || line.startsWith("--MERGE--"))
+				break;
+		m_file->write(line);
+	}
 }
 
 static QByteArray esc(QString text) {
@@ -44,7 +56,9 @@ static QByteArray esc(QString text) {
 
 void HtmlExporter::beginTable(TableCellIterator *iterator)
 {
-	m_file->write("\n<table><thead>\n\t<tr>\n");
+	m_file->write("<div class=\"query\">\n<p class=\"query\">");
+	m_file->write(esc(iterator->query()));
+	m_file->write("</p>\n<table><thead>\n\t<tr>\n");
 	for(int i=0;i<iterator->columns();++i) {
 		m_file->write("\t\t<th>");
 		m_file->write(esc(iterator->header(i).name()));
@@ -60,11 +74,16 @@ void HtmlExporter::beginTable(TableCellIterator *iterator)
 		}
 		m_file->write("\t</tr>\n");
 	}
-	m_file->write("</tbody></table>\n");
+	m_file->write("</tbody></table>\n</div>\n");
 }
 
 void HtmlExporter::done()
 {
-	m_file->write("\n</body></html>");
+	char buffer[1024];
+	int len;
+	while((len=m_tpl->read(buffer, sizeof buffer))>0)
+			m_file->write(buffer, len);
+	delete m_tpl;
+	m_tpl = 0;
 }
 
